1972747f041904c54cf65de90820ec519f7e1fbc
Artiom Tofan <artiomtofancv@gmail.com>
2020-06-13 23:43:41 +0200

    added 'add extra ingridient to modal'

--- components/cocktail-create-form.js
+++ components/cocktail-create-form.js
@@ -1,12 +1,25 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
-
 const CocktailCreateForm = (props) => {
 	const defaultData = {
 		strDrink: '',
 		strIngredient1: '',
 		strMeasure1: '',
+		strIngredient1: '',
+		strMeasure1: '',
+		strIngredient2: '',
+		strMeasure2: '',
+		strIngredient3: '',
+		strMeasure3: '',
+		strIngredient4: '',
+		strMeasure4: '',
+		strIngredient5: '',
+		strMeasure5: '',
+		strIngredient6: '',
+		strMeasure6: '',
+		strIngredient7: '',
+		strMeasure7: '',
 		strDrinkThumb: '',
 		strInstructions: '',
 		strTags: ''
@@ -41,6 +54,15 @@ const CocktailCreateForm = (props) => {
 		});
 	};
 
+	const addMore = (event) => {
+		const clicked = event.target.parentNode.nextElementSibling;
+		clicked.classList.toggle('hidden');
+		clicked.getElementsByTagName('input')[0].setAttribute('required', 'required');
+		clicked.getElementsByTagName('input')[1].setAttribute('required', 'required');
+		clicked.previousSibling.getElementsByTagName('span')[0].classList.toggle('fa-plus')
+		clicked.previousSibling.getElementsByTagName('span')[0].classList.toggle('fa-minus')
+	};
+
 	const submitForm = (event) => {
 		event.preventDefault();
 		const isValid = event.target.reportValidity(); // returns true or false
@@ -53,201 +75,232 @@ const CocktailCreateForm = (props) => {
 	return (
 		<form noValidate onSubmit={submitForm}>
 			<div className="form-group">
-					<label htmlFor="name">Name</label>
-					<input
-						onChange={handleChange}
-						value={form.strDrink}
-						name="strDrink"
-						type="text"
-						className="form-control"
-						id="strDrink"
-						pattern="[a-zA-Z]*"
-						minLength="2"
-						aria-describedby="emailHelp"
-						placeholder="Mojito"
-						autoComplete="on"
-						required
-					/>
+				<label htmlFor="name">Name</label>
+				<input
+					onChange={handleChange}
+					value={form.strDrink}
+					name="strDrink"
+					type="text"
+					className="form-control"
+					id="strDrink"
+					pattern="[a-zA-Z]*"
+					minLength="2"
+					aria-describedby="emailHelp"
+					placeholder="Mojito"
+					autoComplete="on"
+					required
+				/>
 			</div>
-			<div className="form-group">		
-					<label htmlFor="ingredient">Ingredient</label>
-					<input
-						onChange={handleChange}
-						value={form.strIngredient1}
-						name="strIngredient1"
-						type="text"
-						pattern="[a-zA-Z]*"
-						minLength="2"
-						className="form-control"
-						id="strIngredient1"
-						autoComplete="on"
-						placeholder="Light Rum, Vodka"
-						required
-					/>
+			<div className="form-group" id="a">
+				<label htmlFor="ingredient">Ingredient</label>
+				<span className="addMore float-right fas fa-plus" onClick={addMore}>
+				</span>
+				<input
+					onChange={handleChange}
+					value={form.strIngredient1}
+					name="strIngredient1"
+					type="text"
+					pattern="[a-zA-Z]*"
+					minLength="2"
+					className="form-control"
+					id="strIngredient1"
+					autoComplete="on"
+					placeholder="Light Rum, Vodka"
+					required
+				/>
+				{/* </div> */}
+				{/* <div className="form-group"> */}
+				<label htmlFor="measure">Measure</label>
+				<input
+					onChange={handleChange}
+					value={form.strMeasure1}
+					name="strMeasure1"
+					type="text"
+					className="form-control"
+					id="strMeasure1"
+					minLength="2"
+					placeholder="2 oz, 1 cl, 1/2 ml, 3 spoons"
+					required
+				/>
 			</div>
-			<div className="form-group">
-					<label htmlFor="measure">Measure</label>
-					<input
-						onChange={handleChange}
-						value={form.strMeasure1}
-						name="strMeasure1"
-						type="text"
-						className="form-control"
-						id="strMeasure1"
-						minLength="2"
-						placeholder="2 oz, 1 cl, 1/2 ml, 3 spoons"
-						required
-					/>
+			<div className="form-group hidden">
+				<label htmlFor="ingredient">Ingredient 2</label>
+				<span className="addMore float-right fas fa-plus" onClick={addMore}>
+				</span>
+				<input
+					onChange={handleChange}
+					value={form.strIngredient2}
+					name="strIngredient2"
+					type="text"
+					pattern="[a-zA-Z]*"
+					minLength="2"
+					className="form-control"
+					autoComplete="on"
+					placeholder="Light Rum, Vodka"
+				/>
+				{/* </div>
+			<div className="form-group" id="strMeasure2"> */}
+				<label htmlFor="measure">Measure 2</label>
+				<input
+					onChange={handleChange}
+					value={form.strMeasure2}
+					name="strMeasure2"
+					type="text"
+					className="form-control"
+					minLength="2"
+					placeholder="2 oz, 1 cl, 1/2 ml, 3 spoons"
+				/>
 			</div>
-			<div className="form-group">		
-					<label htmlFor="ingredient">Ingredient 2</label>
-					<input
-						onChange={handleChange}
-						value={form.strIngredient2}
-						name="strIngredient2"
-						type="text"
-						pattern="[a-zA-Z]*"
-						minLength="2"
-						className="form-control"
-						id="strIngredient2"
-						autoComplete="on"
-						placeholder="Light Rum, Vodka"
-						required
-					/>
+			<div className="form-group hidden">
+				<label htmlFor="ingredient">Ingredient 3</label>
+				<span className="addMore float-right fas fa-plus" onClick={addMore}>
+				</span>
+				<input
+					onChange={handleChange}
+					value={form.strIngredient3}
+					name="strIngredient3"
+					type="text"
+					pattern="[a-zA-Z]*"
+					minLength="2"
+					className="form-control"
+					id="strIngredient3"
+					autoComplete="on"
+					placeholder="Light Rum, Vodka"
+				/>
+				{/* </div>
+			<div className="form-group">*/}
+				<label htmlFor="measure">Measure 3</label>
+				<input
+					onChange={handleChange}
+					value={form.strMeasure3}
+					name="strMeasure3"
+					type="text"
+					className="form-control"
+					id="strMeasure3"
+					minLength="2"
+					placeholder="2 oz, 1 cl, 1/2 ml, 3 spoons"
+				/>
 			</div>
-			<div className="form-group">
-					<label htmlFor="measure">Measure 2</label>
-					<input
-						onChange={handleChange}
-						value={form.strMeasure2}
-						name="strMeasure2"
-						type="text"
-						className="form-control"
-						id="strMeasure2"
-						minLength="2"
-						placeholder="2 oz, 1 cl, 1/2 ml, 3 spoons"
-						required
-					/>
+			<div className="form-group hidden">
+				<label htmlFor="ingredient">Ingredient 4</label>
+				<span className="addMore float-right fas fa-plus" onClick={addMore}>
+				</span>
+				<input
+					onChange={handleChange}
+					value={form.strIngredient4}
+					name="strIngredient4"
+					type="text"
+					pattern="[a-zA-Z]*"
+					minLength="2"
+					className="form-control"
+					id="strIngredient4"
+					autoComplete="on"
+					placeholder="Light Rum, Vodka"
+				/>
+				{/* </div>
+			<div className="form-group"> */}
+				<label htmlFor="measure">Measure 4</label>
+				<input
+					onChange={handleChange}
+					value={form.strMeasure4}
+					name="strMeasure4"
+					type="text"
+					className="form-control"
+					id="strMeasure4"
+					minLength="2"
+					placeholder="2 oz, 1 cl, 1/2 ml, 3 spoons"
+				/>
 			</div>
-			<div className="form-group">		
-					<label htmlFor="ingredient">Ingredient 3</label>
-					<input
-						onChange={handleChange}
-						value={form.strIngredient3}
-						name="strIngredient3"
-						type="text"
-						pattern="[a-zA-Z]*"
-						minLength="2"
-						className="form-control"
-						id="strIngredient3"
-						autoComplete="on"
-						placeholder="Light Rum, Vodka"
-						required
-					/>
+			<div className="form-group hidden">
+				<label htmlFor="ingredient">Ingredient 5</label>
+				<span className="addMore float-right fas fa-plus" onClick={addMore}>
+				</span>
+				<input
+					onChange={handleChange}
+					value={form.strIngredient5}
+					name="strIngredient5"
+					type="text"
+					pattern="[a-zA-Z]*"
+					minLength="2"
+					className="form-control"
+					id="strIngredient5"
+					autoComplete="on"
+					placeholder="Light Rum, Vodka"
+				/>
+				{/* </div>
+			<div className="form-group"> */}
+				<label htmlFor="measure">Measure 5</label>
+				<input
+					onChange={handleChange}
+					value={form.strMeasure5}
+					name="strMeasure5"
+					type="text"
+					className="form-control"
+					id="strMeasure5"
+					minLength="2"
+					placeholder="2 oz, 1 cl, 1/2 ml, 3 spoons"
+				/>
 			</div>
-			<div className="form-group">
-					<label htmlFor="measure">Measure 3</label>
-					<input
-						onChange={handleChange}
-						value={form.strMeasure3}
-						name="strMeasure3"
-						type="text"
-						className="form-control"
-						id="strMeasure3"
-						minLength="2"
-						placeholder="2 oz, 1 cl, 1/2 ml, 3 spoons"
-						required
-					/>
+			<div className="form-group hidden">
+				<label htmlFor="ingredient">Ingredient 6</label>
+				<span className="addMore float-right fas fa-plus" onClick={addMore}>
+				</span>
+				<input
+					onChange={handleChange}
+					value={form.strIngredient6}
+					name="strIngredient6"
+					type="text"
+					pattern="[a-zA-Z]*"
+					minLength="2"
+					className="form-control"
+					id="strIngredient6"
+					autoComplete="on"
+					placeholder="Light Rum, Vodka"
+				/>
+				{/* </div>
+			<div className="form-group"> */}
+				<label htmlFor="measure">Measure 6</label>
+				<input
+					onChange={handleChange}
+					value={form.strMeasure6}
+					name="strMeasure6"
+					type="text"
+					className="form-control"
+					id="strMeasure6"
+					minLength="2"
+					placeholder="2 oz, 1 cl, 1/2 ml, 3 spoons"
+				/>
 			</div>
-			<div className="form-group">		
-					<label htmlFor="ingredient">Ingredient 4</label>
-					<input
-						onChange={handleChange}
-						value={form.strIngredient4}
-						name="strIngredient4"
-						type="text"
-						pattern="[a-zA-Z]*"
-						minLength="2"
-						className="form-control"
-						id="strIngredient4"
-						autoComplete="on"
-						placeholder="Light Rum, Vodka"
-						required
-					/>
-			</div>
-			<div className="form-group">
-					<label htmlFor="measure">Measure 4</label>
-					<input
-						onChange={handleChange}
-						value={form.strMeasure4}
-						name="strMeasure4"
-						type="text"
-						className="form-control"
-						id="strMeasure4"
-						minLength="2"
-						placeholder="2 oz, 1 cl, 1/2 ml, 3 spoons"
-						required
-					/>
-			</div>
-			<div className="form-group">		
-					<label htmlFor="ingredient">Ingredient 5</label>
-					<input
-						onChange={handleChange}
-						value={form.strIngredient5}
-						name="strIngredient5"
-						type="text"
-						pattern="[a-zA-Z]*"
-						minLength="2"
-						className="form-control"
-						id="strIngredient5"
-						autoComplete="on"
-						placeholder="Light Rum, Vodka"
-						required
-					/>
-			</div>
-			<div className="form-group">
-					<label htmlFor="measure">Measure 5</label>
-					<input
-						onChange={handleChange}
-						value={form.strMeasure5}
-						name="strMeasure5"
-						type="text"
-						className="form-control"
-						id="strMeasure5"
-						minLength="2"
-						placeholder="2 oz, 1 cl, 1/2 ml, 3 spoons"
-						required
-					/>
-			</div>
-			<div className="form-group">		
-					<label htmlFor="ingredient">Ingredient 6</label>
-					<input
-						onChange={handleChange}
-						value={form.strIngredient6}
-						name="strIngredient6"
-						type="text"
-						pattern="[a-zA-Z]*"
-						minLength="2"
-						className="form-control"
-						id="strIngredient6"
-						autoComplete="on"
-						placeholder="Light Rum, Vodka"
-						required
-					/>
-			</div>
-			<div className="form-group">
-					<label htmlFor="measure">Measure 6</label>
-					<input
-						onChange={handleChange}
-						value={form.strMeasure6}
-						name="strMeasure6"
-						type="text"
-						className="form-control"
-						id="strMeasure6"
-						minLength="2"
-						placeholder="2 oz, 1 cl, 1/2 ml, 3 spoons"
-						required
-					/>
+			<div className="form-group hidden">
+				<label htmlFor="ingredient">Ingredient 7</label>
+				{/* <span className="addMore float-right" onClick={addMore}>				
+					+
+				</span> */}
+				<input
+					onChange={handleChange}
+					value={form.strIngredient7}
+					name="strIngredient7"
+					type="text"
+					pattern="[a-zA-Z]*"
+					minLength="2"
+					className="form-control"
+					id="strIngredient7"
+					autoComplete="on"
+					placeholder="Light Rum, Vodka"
+				/>
+				{/* </div>
+			<div className="form-group"> */}
+				<label htmlFor="measure">Measure 7</label>
+				<input
+					onChange={handleChange}
+					value={form.strMeasure7}
+					name="strMeasure7"
+					type="text"
+					className="form-control"
+					id="strMeasure7"
+					minLength="2"
+					placeholder="2 oz, 1 cl, 1/2 ml, 3 spoons"
+				/>
 			</div>
 			{/* <div className="form-group">
 				<label htmlFor="measure">Measure</label>
@@ -319,6 +372,9 @@ const CocktailCreateForm = (props) => {
 					#button_create {
 						position: absolute;
 						bottom: -55px;
+					}
+					.addMore {
+						cursor: pointer;
 					}
 				`}
 			</style>
--- pages/_app.js
+++ pages/_app.js
@@ -30,6 +30,11 @@ class CocktailApp extends App {
 						integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
 						crossOrigin="anonymous"
 					/>
+					<link
+						rel="stylesheet"
+						href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css"
+						crossOrigin="anonymous"
+					/>
 					<script
 						src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
 						integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
